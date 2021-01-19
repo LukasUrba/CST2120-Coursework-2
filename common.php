7c143befdbc1097d7422c2b3ccef427913276ff7
Lukas Urba <lu045@live.mdx.ac.uk>
2021-01-19 14:52:02 +0000

    addition of logo and css

--- common.php
+++ common.php
@@ -7,10 +7,42 @@ function headData($title) {
     echo '<head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title> .$title. </title>
+            <link rel="stylesheet" href="Styles/style.css"> 
+            <title>' .$title. '</title>
             </head>';
-    echo 'body';
+    echo '<body>';
 
+}
+
+function webHeader($pageName) {
+    echo '<div id="header">';
+    echo '<img id="logo" src="/Images/Logo2.png" alt="Logo">';
+
+    $leftLinkNames = array("Home","Search");
+    $leftLinkAddress = array("index.php","search.php");
+    //"Register", "Profile"
+    //"register.php","profile.php"
+    for($x = 0; $x < count($leftLinkNames); $x++) {
+        echo '<a class="headerLinks leftLinks" ';
+        if ($leftLinkNames[$x] == $pageName){
+            echo 'id="selected" ';
+        }
+        echo 'href ="' .$leftLinkAddress[$x]. '">' .$leftLinkNames[$x]. '</a>';
+    }
+
+    echo '<a class="headerLinks rigthLinks" ';
+    if($pageName == "Basket") {
+        echo 'id="selected" ';
+    }
+    echo 'href ="basket.php">Basket</a>';
+
+    echo '<a class="headerLinks rigthLinks" ';
+    if($pageName == "Profile") {
+        echo 'id="selected" ';
+    }
+    echo 'href ="profile.php">Profile</a>';
+
+    echo '</div>';
 }
 
 ?>
