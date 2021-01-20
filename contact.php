8a46732a960463f4cda7ff92edf3edc3ae58b331
Bilal Abdi <bilal.abdi210@gmail.com>
2021-01-20 16:44:39 +0000

    Contact information page added

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Form</title>
+</head>
+<body>
+
+ <main>
+    <p>Send Email</p>
+    <form class="contact-form" action="Contact Information.php" method="post">
+    <input type="text" name="name"placeholder="Full Name">
+    <input type="text" name="mail"placeholder="Your Email">
+    <input type="text" name="subject"placeholder="Subject matter">
+    <textarea name="message" placeholder="Message"></textarea>
+    <button type="submit" name="submit">Send form</button>
+ </main>
+
+</body>
+</html>
